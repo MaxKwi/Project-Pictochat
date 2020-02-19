443264493926c897111db708e4e538bd156a3d69
Zywicki <zywicki1716@students.d211.org>
2020-02-18 18:57:39 -0600

    implemented the paintview and the feed

--- app/src/main/java/com/example/firebaseimagetest/CreateActivity.java
+++ app/src/main/java/com/example/firebaseimagetest/CreateActivity.java
@@ -1,15 +1,70 @@
 package com.example.firebaseimagetest;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.MenuItem;
+import android.webkit.MimeTypeMap;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
 
 public class CreateActivity extends AppCompatActivity {
+
+    private PaintView paintView;
+
+    private static final String TAG = "MainActivity";
+    private RelativeLayout mLayout;
+    int mDefaultColor;
+    int newColor;
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+    //private static final String TAG = "MainActivity";
+
+//    private Button mButtonChooseImage;
+//    private Button mButtonUpload;
+//    private TextView mTextViewShowUploads;
+//    private EditText mEditTextFileName;
+//    private ImageView mImageView;
+//    private ProgressBar mProgressBar;
+
+    private Uri mImageUri;
+
+    private StorageReference mStorageRef;
+    private DatabaseReference mDatabaseRef;
+
+    private StorageTask mUploadTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,5 +99,167 @@ public class CreateActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        paintView = (PaintView) findViewById(R.id.paintView);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        paintView.init(metrics);
+
+
+        mLayout = (RelativeLayout) findViewById(R.id.activity_main);
+        mDefaultColor = ContextCompat.getColor(CreateActivity.this, R.color.colorPrimary);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference("uploads"); //string is the location in the string
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("uploads");
+
+    }
+
+    private void openFileChooser(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null)
+        {
+            mImageUri = data.getData();
+
+            //Picasso.with(this).load(mImageUri).into(mImageView);
+        }
+    }
+
+    private String getFileExtension(Uri uri){
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType((cR.getType(uri)));
+    }
+
+    private void uploadFile(){
+
+        if (mImageUri != null){
+            StorageReference fileReference = mStorageRef.child(System.currentTimeMillis() + "." + getFileExtension(mImageUri));
+
+            mUploadTask = fileReference.putFile(mImageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    //mProgressBar.setProgress(0);
+                                }
+                            }, 5000);
+
+                            Toast.makeText(CreateActivity.this, "Upload successful", Toast.LENGTH_LONG).show();
+
+//                            Upload upload =  new Upload(mEditTextFileName.getText().toString().trim(), taskSnapshot.getUploadSessionUri().toString());
+//                            String uploadId = mDatabaseRef.push().getKey();
+//                            mDatabaseRef.child(uploadId).setValue(upload);
+
+                            Task<Uri> urlTask = taskSnapshot.getStorage().getDownloadUrl();
+                            while (!urlTask.isSuccessful());
+                            Uri downloadUrl = urlTask.getResult();
+
+                            //Log.d(TAG, "onSuccess: firebase download url: " + downloadUrl.toString()); //use if testing...don't need this line.
+                            //Upload upload = new Upload(mEditTextFileName.getText().toString().trim(),downloadUrl.toString());
+
+                            Upload upload = new Upload("picture",downloadUrl.toString());
+
+                            String uploadId = mDatabaseRef.push().getKey();
+                            mDatabaseRef.child(uploadId).setValue(upload);
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(CreateActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                            //mProgressBar.setProgress((int)progress);
+                        }
+                    });
+        }
+
+
+
+        else{
+            Toast.makeText(this, "No file selected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "picture", null);
+        return Uri.parse(path);
+    }
+
+    private void UploadImage()
+    {
+        paintView.buildDrawingCache();
+        Bitmap image = paintView.getmBitmap();
+        mImageUri = getImageUri(this, image);
+        uploadFile();
+    }
+
+    private void SaveImage()
+    {
+        paintView.buildDrawingCache();
+        Bitmap image = paintView.getmBitmap();
+        if(isStoragePermissionGranted()) {
+            MediaStore.Images.Media.insertImage(getContentResolver(), image, "picture", "picture");  // Saves the image.
+            Toast.makeText(this, "Saved to gallery.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public  boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Permission is granted");
+                return true;
+            } else {
+
+                Log.v(TAG,"Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG,"Permission is granted");
+            return true;
+        }
+    }
+
+
+
+    private void CallColorPicker()
+    {
+
+        AmbilWarnaDialog dialog = new AmbilWarnaDialog(this, mDefaultColor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                newColor = color;
+            }
+        });
+
+        dialog.show();
+
     }
 }
--- app/src/main/java/com/example/firebaseimagetest/ImagesActivity.java
+++ app/src/main/java/com/example/firebaseimagetest/ImagesActivity.java
@@ -5,14 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -39,7 +42,38 @@ public class ImagesActivity extends AppCompatActivity implements ImageAdapter.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_images);
+        setContentView(R.layout.activity_feed);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.nav_feed);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        startActivity(new Intent(getApplicationContext()
+                                , MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.nav_draw:
+                        startActivity(new Intent(getApplicationContext()
+                                , CreateActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.nav_feed:
+                        return true;
+                    case R.id.nav_saved:
+                        startActivity(new Intent(getApplicationContext()
+                                , SavedActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+
+
 
         mProgressCircle = findViewById(R.id.progress_circle);
 
